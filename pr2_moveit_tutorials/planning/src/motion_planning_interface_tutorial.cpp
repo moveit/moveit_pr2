9796608d8f1c0f4857683ed131ebcf88aa4b476b
isucan <isucan@gmail.com>
2013-09-03 20:46:40 +0300

    add forgotten include file

--- pr2_moveit_tutorials/planning/src/motion_planning_interface_tutorial.cpp
+++ pr2_moveit_tutorials/planning/src/motion_planning_interface_tutorial.cpp
@@ -40,6 +40,7 @@
 // MoveIt!
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/planning_interface/planning_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <moveit_msgs/PlanningScene.h>
