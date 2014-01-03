f69c4024e667e51c2b14ad3ba6230f1b8ca097dc
Acorn Pooley <acorn.pooley@sri.com>
2014-01-03 12:31:21 -0800

    python tutorial: add go command

--- pr2_moveit_tutorials/planning/scripts/move_group_python_interface_tutorial.py
+++ pr2_moveit_tutorials/planning/scripts/move_group_python_interface_tutorial.py
@@ -34,81 +34,138 @@
 #
 # Author: Acorn Pooley
 
+## BEGIN_SUB_TUTORIAL imports
+##
+## To use the python interface to move_group, import the moveit_commander
+## module.  We also import rospy and some messages that we will use.
 import sys
 import rospy
 import moveit_commander
-#from moveit_commander import RobotCommander, roscpp_initialize, roscpp_shutdown
-from moveit_msgs.msg import RobotState
+from moveit_msgs.msg import RobotState, DisplayTrajectory
+from geometry_msgs.msg importo Pose, PoseStamped
+## END_SUB_TUTORIAL
 
 def move_group_python_interface_tutorial():
-  # BEGIN_TUTORIAL
-  #
-  # Setup
-  # ^^^^^
-  #
-  # First initialize moveit_commander and rospy.
+  ## BEGIN_TUTORIAL
+  ##
+  ## Setup
+  ## ^^^^^
+  ## CALL_SUB_TUTORIAL imports
+  ##
+  ## First initialize moveit_commander and rospy.
   moveit_commander.roscpp_initialize(sys.argv)
   rospy.init_node('move_group_python_interface_tutorial',
                   anonymous=True)
 
-  # This sleep is ONLY to allow Rviz to come up
+  ## Wait for RVIZ to initialize. This sleep is ONLY to allow Rviz to come up.
   print "============ Waiting for RVIZ..."
   rospy.sleep(20)
   print "============ Starting tutorial "
 
-  # Instantiate a RobotCommander object.  This object is an interface to
-  # the robot as a whole.
+  ## Instantiate a RobotCommander object.  This object is an interface to
+  ## the robot as a whole.
   robot = moveit_commander.RobotCommander()
 
-  # Instantiate a PlanningSceneInterface object.  This object is an interface
-  # to the world surrounding the robot.
+  ## Instantiate a PlanningSceneInterface object.  This object is an interface
+  ## to the world surrounding the robot.
   scene = moveit_commander.PlanningSceneInterface()
 
-  # Instantiate a MoveGroupCommander object.  This object is an interface
-  # to one group of joints.  In this case the group is the joints in the right
-  # arm.  This interface can be used to plan and execute motions on the right
-  # arm.
+  ## Instantiate a MoveGroupCommander object.  This object is an interface
+  ## to one group of joints.  In this case the group is the joints in the left
+  ## arm.  This interface can be used to plan and execute motions on the left
+  ## arm.
   group = moveit_commander.MoveGroupCommander("left_arm")
 
 
-  # Getting Basic Information
-  # ^^^^^^^^^^^^^^^^^^^^^^^^^
-  #
-  # We can get the name of the reference frame for this robot
+  ## Getting Basic Information
+  ## ^^^^^^^^^^^^^^^^^^^^^^^^^
+  ##
+  ## We can get the name of the reference frame for this robot
   print "============ Reference frame: %s" % group.get_planning_frame()
 
-  # We can also print the name of the end-effector link for this group
+  ## We can also print the name of the end-effector link for this group
   print "============ Reference frame: %s" % group.get_end_effector_link()
 
-  # We can get a list of all the groups in the robot
+  ## We can get a list of all the groups in the robot
   print "============ Robot Groups:"
   print robot.get_group_names()
 
-  # Sometimes for debugging it is useful to print the entire state of the
-  # robot.
+  ## Sometimes for debugging it is useful to print the entire state of the
+  ## robot.
   print "============ Printing robot state"
   print robot.get_current_state()
   print "============"
 
 
-  # Planning to a Pose goal
-  # ^^^^^^^^^^^^^^^^^^^^^^^
-  # We can plan a motion for this group to a desired pose for the 
-  # end-effector
+  ## Planning to a Pose goal
+  ## ^^^^^^^^^^^^^^^^^^^^^^^
+  ## We can plan a motion for this group to a desired pose for the 
+  ## end-effector
+  print "============ Generating plan 1"
+  pose_target = Pose()
+  pose_target.orientation.w = 1.0
+  pose_target.position.x = 0.7
+  pose_target.position.y = -0.05
+  pose_target.position.z = 1.1
+  group.set_pose_target(pose_target)
 
-    
+  ## Now, we call the planner to compute the plan
+  ## and visualize it if successful
+  ## Note that we are just planning, not asking move_group 
+  ## to actually move the robot
+  plan = group.plan()
+
+  print "============ Waiting while RVIZ displays the plan"
+  rospy.sleep(10)
+
+  ## You can ask RVIZ to visualize the trajectory for you, but the group object
+  ## does this by default so it is not necessary right after generating the
+  ## plan.
+  if 1:
+    print "============ Visualizing plan 1"
+    display_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                        DisplayTrajectory)
+    display_trajectory = DisplayTrajectory()
+
+    display_trajectory.trajectory_start = robot.get_current_state()
+    display_trajectory.trajectory.append(plan)
+    display_publisher.publish(display_trajectory);
+
+    print "============ waiting while trajectory is visualized..."
+    rospy.sleep(10)
+  
+
+  ## Moving to a pose goal
+  ## ^^^^^^^^^^^^^^^^^^^^^
+  ##
+  ## Moving to a pose goal is similar to the step above
+  ## except we now use the go() function. Note that
+  ## the pose goal we had set earlier is still active 
+  ## and so the robot will try to move to that goal. We will
+  ## not use that function in this tutorial since it is 
+  ## a blocking function and requires a controller to be active
+  ## and report success on execution of a trajectory.
+
+  # Uncomment below line when working with a real robot
+  # group.go(wait=True)
+
+  ## Planning to a joint-space goal 
+  ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  ##
+  ## Let's set a joint space goal and move towards it. 
+  ## First, we will clear the pose target we had just set.
+
+  group.clear_pose_targets()
 
 
 
 
 
 
-
-
-  # When finished shut down moveit_commander.
+  ## When finished shut down moveit_commander.
   moveit_commander.roscpp_shutdown()
 
-  # END_TUTORIAL
+  ## END_TUTORIAL
 
   print "============ STOPPING"
 
