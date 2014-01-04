6bc1b710d694478b4d933d02590fb6cb1d92f602
Acorn Pooley <acorn.pooley@sri.com>
2014-01-03 16:39:30 -0800

    python tutorial: add cartesian paths

--- pr2_moveit_tutorials/planning/scripts/move_group_python_interface_tutorial.py
+++ pr2_moveit_tutorials/planning/scripts/move_group_python_interface_tutorial.py
@@ -39,9 +39,10 @@
 ## To use the python interface to move_group, import the moveit_commander
 ## module.  We also import rospy and some messages that we will use.
 import sys
+import copy
 import rospy
 import moveit_commander
-from moveit_msgs.msg import RobotState, DisplayTrajectory
+from moveit_msgs.msg import RobotState, DisplayTrajectory, OrientationConstraint
 from geometry_msgs.msg import Pose, PoseStamped
 ## END_SUB_TUTORIAL
 
@@ -131,14 +132,14 @@ def move_group_python_interface_tutorial():
   ## You can ask RVIZ to visualize a plan (aka trajectory) for you.  But the
   ## group.plan() method does this automatically so this is not that useful
   ## here (it just displays the same trajectory again).
-  print "============ Visualizing plan 1"
+  print "============ Visualizing plan1"
   display_trajectory = DisplayTrajectory()
 
   display_trajectory.trajectory_start = robot.get_current_state()
   display_trajectory.trajectory.append(plan1)
   display_trajectory_publisher.publish(display_trajectory);
 
-  print "============ waiting while plan1 is visualized (again)..."
+  print "============ Waiting while plan1 is visualized (again)..."
   rospy.sleep(5)
 
 
@@ -178,6 +179,45 @@ def move_group_python_interface_tutorial():
   print "============ Waiting while RVIZ displays plan2..."
   rospy.sleep(5)
 
+
+  ## Cartesian Paths
+  ## ^^^^^^^^^^^^^^^
+  ## You can plan a cartesian path directly by specifying a list of waypoints 
+  ## for the end-effector to go through.
+  waypoints = []
+
+  # start with the current pose
+  waypoints.append(group.get_current_pose().pose)
+
+  # first orient gripper and move forward (+x)
+  wpose = Pose()
+  wpose.orientation.w = 1.0
+  wpose.position.x = waypoints[0].position.x + 0.1
+  wpose.position.y = waypoints[0].position.y
+  wpose.position.z = waypoints[0].position.z
+  waypoints.append(copy.deepcopy(wpose))
+
+  # second move down
+  wpose.position.z -= 0.10
+  waypoints.append(copy.deepcopy(wpose))
+
+  # third move to the side
+  wpose.position.y += 0.05
+  waypoints.append(copy.deepcopy(wpose))
+
+  ## We want the cartesian path to be interpolated at a resolution of 1 cm
+  ## which is why we will specify 0.01 as the eef_step in cartesian
+  ## translation.  We will specify the jump threshold as 0.0, effectively
+  ## disabling it.
+  (plan3, fraction) = group.compute_cartesian_path(
+                               waypoints,   # waypoints to follow
+                               0.01,        # eef_step
+                               0.0)         # jump_threshold
+                               
+  print "============ Waiting while RVIZ displays plan3..."
+  rospy.sleep(5)
+
+ 
 
 
   ## When finished shut down moveit_commander.
