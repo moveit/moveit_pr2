c7231977d4843bbc584f2888d951b73248223ef5
Dave Hershberger <dave.hershberger@sri.com>
2014-01-15 10:29:42 -0800

    Refactored jacobian test.

--- pr2_moveit_tests/kinematics/src/test_jacobian.cpp
+++ pr2_moveit_tests/kinematics/src/test_jacobian.cpp
@@ -64,8 +64,10 @@ bool NOT_NEAR(const double &v1, const double &v2, const double &NEAR)
    return false;
 }
 
-TEST(JacobianSolver, solver)
+void testJacobian(std::string group_name, std::string base_link, std::string tip_link)
 {
+ SCOPED_TRACE(group_name + ": " + base_link + " to " + tip_link);
+ 
  srand ( time(NULL) ); // initialize random seed:
  rdf_loader::RDFLoader model_loader("robot_description");
  robot_model::RobotModelPtr kinematic_model;
@@ -75,9 +77,9 @@ TEST(JacobianSolver, solver)
  kinematic_state.reset(new robot_state::RobotState(kinematic_model));
  kinematic_state->setToDefaultValues();
 
- const moveit::core::JointModelGroup* joint_state_group = kinematic_state->getRobotModel()->getJointModelGroup("right_arm");
+ const moveit::core::JointModelGroup* joint_state_group = kinematic_state->getRobotModel()->getJointModelGroup(group_name);
 
- std::string link_name = "r_wrist_roll_link";
+ std::string link_name = tip_link;
  std::vector<double> joint_angles(7,0.0);
  geometry_msgs::Point ref_position;
  Eigen::MatrixXd jacobian;
@@ -91,8 +93,8 @@ TEST(JacobianSolver, solver)
    ROS_ERROR("Could not initialize tree object");
  }
  KDL::Chain kdl_chain;
- std::string base_frame("torso_lift_link");
- std::string tip_frame("r_wrist_roll_link");
+ std::string base_frame(base_link);
+ std::string tip_frame(tip_link);
  if (!tree.getChain(base_frame, tip_frame, kdl_chain))
  {
    ROS_ERROR("Could not initialize chain object");
@@ -123,64 +125,14 @@ TEST(JacobianSolver, solver)
  }
 }
 
+TEST(JacobianSolver, solver)
+{
+  testJacobian("right_arm", "torso_lift_link", "r_wrist_roll_link");
+}
+
 TEST(JacobianSolver, solver2)
 {
- srand ( time(NULL) ); // initialize random seed:
- rdf_loader::RDFLoader model_loader("robot_description");
- robot_model::RobotModelPtr kinematic_model;
- kinematic_model.reset(new robot_model::RobotModel(model_loader.getURDF(),model_loader.getSRDF()));
-
- robot_state::RobotStatePtr kinematic_state;
- kinematic_state.reset(new robot_state::RobotState(kinematic_model));
- kinematic_state->setToDefaultValues();
-
- const moveit::core::JointModelGroup* joint_state_group = kinematic_state->getRobotModel()->getJointModelGroup("left_arm");
-
- std::string link_name = "l_wrist_roll_link";
- std::vector<double> joint_angles(7,0.0);
- geometry_msgs::Point ref_position;
- Eigen::MatrixXd jacobian;
- Eigen::Vector3d point(0.0,0.0,0.0);
- kinematic_state->setJointGroupPositions(joint_state_group, &joint_angles[0]);
- ASSERT_TRUE(kinematic_state->getJacobian(joint_state_group, kinematic_state->getRobotModel()->getLinkModel(link_name),point,jacobian));
-
- 
- KDL::Tree tree;
- if (!kdl_parser::treeFromUrdfModel(*model_loader.getURDF(), tree))
- {
-   ROS_ERROR("Could not initialize tree object");
- }
- KDL::Chain kdl_chain;
- std::string base_frame("torso_lift_link");
- std::string tip_frame("l_wrist_roll_link");
- if (!tree.getChain(base_frame, tip_frame, kdl_chain))
- {
-   ROS_ERROR("Could not initialize chain object");
- }
- KDL::ChainJntToJacSolver kdl_solver(kdl_chain);
- KDL::Jacobian jacobian_kdl(7);
- KDL::JntArray q_in(7);
- EXPECT_TRUE(kdl_solver.JntToJac(q_in,jacobian_kdl) >= 0);
-
- unsigned int NUM_TESTS = 10000;
- for(unsigned int i=0; i < NUM_TESTS; i++)
- {
-   for(int j=0; j < 7; j++)
-   {
-     q_in(j) = gen_rand(-M_PI,M_PI);
-     joint_angles[j] = q_in(j);
-   }
-   EXPECT_TRUE(kdl_solver.JntToJac(q_in,jacobian_kdl) >= 0);
-   kinematic_state->setJointGroupPositions(joint_state_group, &joint_angles[0]);
-   EXPECT_TRUE(kinematic_state->getJacobian(joint_state_group, kinematic_state->getRobotModel()->getLinkModel(link_name),point,jacobian));
-   for(unsigned int k=0; k < 6; k++)
-   {
-     for(unsigned int m=0; m < 7; m++)
-     {
-       EXPECT_FALSE(NOT_NEAR(jacobian_kdl(k,m),jacobian(k,m),1e-10));
-     }
-   }
- }
+  testJacobian("left_arm", "torso_lift_link", "l_wrist_roll_link");
 }
 
 int main(int argc, char **argv)
